173b1a9602edd4b4fa921db5d3a79d86a45f54e3
Ductuan <nongductuan10@gmail.com>
2022-12-09 17:14:54 +0700

    init loop

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,7 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-
+//
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "mainloop.h"
